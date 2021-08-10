23b7854d35d2895502f97d3dcfcf8d7541d38f65
2 <1>
2021-08-10 09:30:56 +0800

    feat(deno-gulp): init

---
+++ src/undertaker.js
@@ -0,0 +1,50 @@
+
+// var inherits = require('util').inherits;
+import { extend as inherits } from "https://unpkg.com/lodash-es@4.17.21/lodash.js";
+// var EventEmitter = require('events').EventEmitter;
+import { EventEmitter } from "https://deno.land/std@0.103.0/node/events.ts";
+
+// var DefaultRegistry = require('undertaker-registry');
+import DefaultRegistry from './undertaker-registry.js';
+
+// var tree = require('./lib/tree');
+// var task = require('./lib/task');
+// var series = require('./lib/series');
+// var lastRun = require('./lib/last-run');
+// var parallel = require('./lib/parallel');
+// var registry = require('./lib/registry');
+// var _getTask = require('./lib/get-task');
+// var _setTask = require('./lib/set-task');
+
+function Undertaker(customRegistry) {
+  EventEmitter.call(this);
+
+  this._registry = new DefaultRegistry();
+  if (customRegistry) {
+    this.registry(customRegistry);
+  }
+
+  this._settle = (process.env.UNDERTAKER_SETTLE === 'true');
+}
+
+inherits(Undertaker, EventEmitter);
+
+
+// Undertaker.prototype.tree = tree;
+
+// Undertaker.prototype.task = task;
+
+// Undertaker.prototype.series = series;
+
+// Undertaker.prototype.lastRun = lastRun;
+
+// Undertaker.prototype.parallel = parallel;
+
+// Undertaker.prototype.registry = registry;
+
+// Undertaker.prototype._getTask = _getTask;
+
+// Undertaker.prototype._setTask = _setTask;
+
+// module.exports = Undertaker;
+export default Undertaker;
